08e55c948a0eb7a2f34c5c05466bca457eb13b09
tomytoma <tomytoma@gmail.com>
2020-09-01 14:20:11 +0900

    리셋 버튼 추가

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { CSSTransitionGroup } from 'react-transition-group';
+import { Link } from 'react-router-dom';
 
 function Result(props) {
   return (
@@ -13,8 +14,11 @@ function Result(props) {
       transitionAppear
       transitionAppearTimeout={200}
     >
-      <div>
+      <div className="result_contents">
         <p>당신이 선호하는 투자 분야는 <strong>{props.quizResult}</strong>입니다</p>
+      </div>
+      <div id="intro_button">
+            <Link to="/">리셋</Link>
       </div>
     </CSSTransitionGroup>
   );
