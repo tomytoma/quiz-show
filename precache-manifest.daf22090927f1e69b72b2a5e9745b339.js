c06d6dbe88886bf853d994f8906b7b5e6ce010bc
tomytoma <tomytoma@gmail.com>
2020-09-01 14:12:55 +0900

    Updates

---
+++ precache-manifest.daf22090927f1e69b72b2a5e9745b339.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c68e4fddb26b326d6c4",
+    "url": "/quiz-show/static/css/main.eb04e0f5.chunk.css"
+  },
+  {
+    "revision": "0c68e4fddb26b326d6c4",
+    "url": "/quiz-show/static/js/main.0c68e4fd.chunk.js"
+  },
+  {
+    "revision": "b27523c480e9fc9e4aa4",
+    "url": "/quiz-show/static/js/1.b27523c4.chunk.js"
+  },
+  {
+    "revision": "9da5a8224273cd0d6979",
+    "url": "/quiz-show/static/js/runtime~main.9da5a822.js"
+  },
+  {
+    "revision": "9e00aea3b0f710db13c0f8d577637677",
+    "url": "/quiz-show/static/media/logo.9e00aea3.svg"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/quiz-show/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "03e34242c26dad8ba67b80eb3ec4ac4f",
+    "url": "/quiz-show/index.html"
+  }
+];
